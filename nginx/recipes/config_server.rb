5a0af8be940c846b4e4120e4280f998db1192222
Ryan Geyer <me@ryangeyer.com>
2011-04-05 15:54:31 -0700

    Breaking source for APT, cause that's what matters at the moment

--- nginx/recipes/config_server.rb
+++ nginx/recipes/config_server.rb
@@ -72,7 +72,10 @@ remote_file "#{node[:nginx][:dir]}/mime.types" do
   mode "0644"
 end
 
-runit_service "nginx"
+service "nginx" do
+  supports :status => true, :restart => true, :reload => true
+  action [ :enable, :start ]
+end
 
 # Disable the default site
 nginx_site "default" do
