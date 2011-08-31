570d1b3e5da573a4050e835a16cb4a71a423d3c8
Ryan J. Geyer <me@ryangeyer.com>
2011-08-31 16:22:21 -0700

    Pulled/Merged rightscale master

--- iptables/recipes/default.rb
+++ iptables/recipes/default.rb
@@ -28,10 +28,24 @@ directory "/etc/iptables.d" do
   action :create
 end
 
-cookbook_file "/usr/sbin/rebuild-iptables" do
+remote_file "/usr/sbin/rebuild-iptables" do
   source "rebuild-iptables"
   mode 0755
 end
 
+case node[:platform]
+when "redhat", "centos"
+  iptables_save_file = "/etc/sysconfig/iptables"
+when "ubuntu", "debian"
+  iptables_save_file = "/etc/iptables/general"
+
+  template "/etc/network/if-pre-up.d/iptables_load" do
+    source "iptables_load.erb"
+    mode 0755
+    variables :iptables_save_file => iptables_save_file
+  end
+end
+
 iptables_rule "all_established"
 iptables_rule "all_icmp"
+
